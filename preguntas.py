7413cad6848949ec82c770da7d02bc269be57b34
María Carolina Valencia García <marvalenciaga@unal.edu.co>
2022-05-14 16:58:39 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -66,20 +66,20 @@ def pregunta_01():
     En esta función se realiza la carga de datos.
     """
     # Lea el archivo `mushrooms.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv("mushrooms.csv", sep=",")
+    df = pd.read_csv("mushrooms.csv", sep=",", header=0 )
     
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
-    df.drop("veil_type", inplace=True, axis=1)
+    df.pop("veil_type")
 
     # Asigne la columna `type` a la variable `y`.
-    y = df["type"].values
+    y = df["type"]
 
     # Asigne una copia del dataframe `df` a la variable `X`.
     X = df.copy()
 
     # Remueva la columna `type` del DataFrame `X`.
-    X.drop("type", inplace=True, axis=1)
+    X.pop("type")
 
     # Retorne `X` y `y`
     return X, y
